5cdb8cfebd58416e8a4ff95a204ab14a9ac465fe
Sam <sam@wekk.io>
2024-03-09 20:45:31 +0100

    Cleanup shell profile

--- shell/profile.sh
+++ shell/profile.sh
@@ -12,7 +12,6 @@ load_sh_config() {
 }
 
 load_sh_config "env"
-load_sh_config "detect_os"
 load_sh_config "path"
 load_sh_config "ssh_agent"
 load_sh_config "lc"
@@ -21,20 +20,9 @@ load_sh_config "aliases"
 load_sh_config "prompt"
 load_sh_config "completion"
 
-# Eventually source Nix own env
-nix_env_path="${HOME}/.nix-profile/etc/profile.d/nix.sh"
-[ -e "${nix_env_path}" ] && . "${nix_env_path}"
-unset nix_env_path
-
 # sometimes the TERM variable is not really pretty
 if [ "${TERM}" = xterm ] || [ "${TERM}" = vt220 ]; then
     export TERM="xterm-256color"
 fi
 
-# Enventually set LXC infos
-is_on_lxc && load_sh_config 'lxc'
-
-which zellij 2>&1 >/dev/null && load_sh_config "zellij"
-
-# Welcome message when ready
-load_sh_config "welcome"
+command -v zellij 2>&1 >/dev/null && load_sh_config "zellij"
