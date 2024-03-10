7ad64b3430ea489a02ea6f8bc541cdabce246a90
Sam <sam@wekk.io>
2024-03-10 13:41:53 +0100

    Fix zellij shortcut

--- bin/zellij-shortcut.sh
+++ bin/zellij-shortcut.sh
@@ -1,25 +1,23 @@
 #!/usr/bin/env sh
 
-function zj() {
-  SESSIONS_LIST="$(zellij list-sessions --no-formatting | grep -vF EXITED | sed -e 's/(current)//g' | tr -s '[:blank:]')"
+SESSIONS_LIST="$(zellij list-sessions --no-formatting | grep -vF EXITED | sed -e 's/(current)//g' | tr -s '[:blank:]')"
 
-  if [ ! -z "${SESSIONS_LIST}" ]; then
-    if which sk 2>&1 >/dev/null; then
-      if [ -z "${1}" ]; then
-        zellij attach "$(echo "${SESSIONS_LIST}" | sk)"
-      else
-        SELECTED_SESSION="$(echo "${SESSIONS_LIST}" | sk -0 -1 --query "${1}")"
-
-        if [ -z "${SELECTED_SESSION}" ]; then
-          zellij attach --create "${1}"
-        else
-          zellij attach "${SELECTED_SESSION}"
-        fi
-      fi
+if [ ! -z "${SESSIONS_LIST}" ]; then
+  if command -v skim 2>&1 >/dev/null; then
+    if [ -z "${1}" ]; then
+      zellij attach "$(echo "${SESSIONS_LIST}" | sk)"
     else
-      echo "${SESSIONS_LIST}"
+      SELECTED_SESSION="$(echo "${SESSIONS_LIST}" | sk -0 -1 --query "${1}")"
+
+      if [ -z "${SELECTED_SESSION}" ]; then
+        zellij attach --create "${1}"
+      else
+        zellij attach "${SELECTED_SESSION}"
+      fi
     fi
-  elif [ ! -z "${1}" ]; then
-    zellij attach --create "${1}"
+  else
+    echo "${SESSIONS_LIST}"
   fi
-}
+elif [ ! -z "${1}" ]; then
+  zellij attach --create "${1}"
+fi
