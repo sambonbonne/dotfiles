8d52d2ae7330aef13d4d58164a0e8f9fa8e4b355
Sam <sam@wekk.io>
2024-03-13 14:10:39 +0100

    Improve completion generation

--- shell/profile.d/completion.sh
+++ shell/profile.d/completion.sh
@@ -1,22 +1,27 @@
-#!/usr/bin/env sh
-
 SHELL_BIN="$(ps -p $$ -ocomm=)"
 
+source_completion() {
+  if command -v "${1}" 2>&1 >/dev/null; then
+    source <(eval "$@") >/dev/null
+  fi
+}
+
 # Kubernetes completions
-command -v kubectl >/dev/null && source <(kubectl completion "${SHELL_BIN}") # TODO not found compdef
-command -v kustomize >/dev/null && source <(kustomize completion "${SHELL_BIN}")
-command -v helm >/dev/null && source <(helm completion "${SHELL_BIN}")
-command -v k9s >/dev/null && source <(k9s completion "${SHELL_BIN}")
+source_completion kubectl completion "${SHELL_BIN}"
+source_completion kustomize completion "${SHELL_BIN}"
+source_completion k9s completion "${SHELL_BIN}"
+source_completion helm completion "${SHELL_BIN}"
 
 # Kubernetes tools completions
-command -v argocd >/dev/null && source <(argocd completion "${SHELL_BIN}")
-command -v argocd-autopilot >/dev/null && source <(argocd-autopilot completion "${SHELL_BIN}")
-command -v flux >/dev/null && source <(flux completion "${SHELL_BIN}")
-command -v pluto >/dev/null && source <(pluto completion "${SHELL_BIN}")
-command -v nova >/dev/null && source <(nova completion "${SHELL_BIN}")
+source_completion argocd completion "${SHELL_BIN}"
+source_completion argocd-autopilot completion "${SHELL_BIN}"
+source_completion flux completion "${SHELL_BIN}"
+source_completion pluto completion "${SHELL_BIN}" 2>/dev/null # remove Fairwinds Insights message
+source_completion nova completion "${SHELL_BIN}"
 
 # Cloud CLI completions
-command -v scw >/dev/null && eval "$(scw autocomplete script shell="${SHELL_BIN}")"
+source_completion scw autocomplete script "shell=${SHELL_BIN}"
 
 # Other tooling completion
-command -v just >/dev/null && source <(just --completions "${SHELL_BIN}")
+source_completion just --completions "${SHELL_BIN}"
+source_completion dotter gen-completions --shell "${SHELL_BIN}"
