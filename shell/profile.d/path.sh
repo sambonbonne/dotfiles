40549d1eccaff7ceef1f73e8b3516092571d06e9
Sam <sam@wekk.io>
2024-03-13 13:31:44 +0100

    Shells: fix indentation

--- shell/profile.d/path.sh
+++ shell/profile.d/path.sh
@@ -3,18 +3,18 @@
 # Add some files in path when needed
 
 str_contains() {
-    local string="${1}"
-	local substring="${2}"
-	if test "${string#*$substring}" != "${string}"; then
-		return 0
-	else
-		return 1
-	fi
+  local string="${1}"
+  local substring="${2}"
+  if test "${string#*$substring}" != "${string}"; then
+    return 0
+  else
+    return 1
+  fi
 }
 
 # Append if exists and not already in path
 path_append() {
-	[ -d "${1}" ] && (! str_contains "${PATH}" "${1}") && PATH="${1}:${PATH}"
+  [ -d "${1}" ] && (! str_contains "${PATH}" "${1}") && PATH="${1}:${PATH}"
 }
 
 # global
@@ -29,5 +29,5 @@ path_append "${HOME}/.npm/bin"
 path_append "${HOME}/.composer/vendor/bin"
 
 if [ -d "${HOME}/.sdkman" ] && [ -s "${HOME}/.sdkman/bin/sdkman-init.sh" ]; then
-	source "${HOME}/.sdkman/bin/sdkman-init.sh"
+  source "${HOME}/.sdkman/bin/sdkman-init.sh"
 fi
