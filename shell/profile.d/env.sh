3f5e7f21dd00cd3c538ff2a9255a8f369f2e21c9
Sam <sam@candy.consulting>
2025-08-06 08:53:25 +0200

    Shell environment: add DOCKER_HOST environment variable

--- shell/profile.d/env.sh
+++ shell/profile.d/env.sh
@@ -1,3 +1,7 @@
 #!/usr/bin/env sh
 
 export GOPATH="${HOME}/.local/go"
+
+if [ ! -S /var/run/docker.sock ] && [ -S "${XDG_RUNTIME_DIR}/podman/podman.sock" ]; then
+  export DOCKER_HOST="unix://${XDG_RUNTIME_DIR}/podman/podman.sock"
+fi
