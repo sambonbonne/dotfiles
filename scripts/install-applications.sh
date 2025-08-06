53cc901966da8e9152d205fbb2fa72769e8de7bb
Sam <sam@candy.consulting>
2025-08-06 08:50:47 +0200

    Flatpak installation: remove some applications and add Junction

--- scripts/install-applications.sh
+++ scripts/install-applications.sh
@@ -5,15 +5,14 @@ set -e
 flatpak remote-add --user --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
 
 flatpak install --user --noninteractive flathub \
+  re.sonny.Junction \
   org.gnome.Loupe org.gnome.Papers \
   org.gnome.Contacts org.gnome.Calendar \
   com.github.neithern.g4music org.videolan.VLC com.github.KRTirtho.Spotube \
-  io.gitlab.librewolf-community com.github.Eloston.UngoogledChromium \
+  io.gitlab.librewolf-community \
   org.ferdium.Ferdium \
   org.libreoffice.LibreOffice org.onlyoffice.desktopeditors \
-  io.github.alexkdeveloper.calculator \
   org.torproject.torbrowser-launcher \
-  ch.protonmail.protonmail-bridge \
   me.kozec.syncthingtk org.keepassxc.KeePassXC \
   app.organicmaps.desktop \
   org.gnome.Firmware
